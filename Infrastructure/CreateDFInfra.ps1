6cda32c52ef91ff4de3fc8fde36785a855962cff
josh.lefebvre@lefewaresolutions.com <josh.lefebvre@lefewaresolutions.com>
2023-02-19 20:43:25 -0600

    Fix KV Baseurls

--- Infrastructure/CreateDFInfra.ps1
+++ Infrastructure/CreateDFInfra.ps1
@@ -1,33 +1,23 @@
 $subscriptionName = 'LefeWareSolutions-POC'
 $location = "centralus"
-$environment = "dev"
-$resourceGroupName = "lws-cus-$($environment)-dfcicd-rg"
+$locationShort = "cus"
+$environment = "stg"
+$orgNameShort = "lws" 
+$resourceGroupName = "$($orgNameShort)-$($locationShort)-$($environment)-dfcicd-rg"
 
-
-
-# Connect to Azure 
+# Connect to Azure and Set Current Subscription Context
 Connect-AzAccount
-
-# Set Current Subscription Context
 $context = Get-AzSubscription -SubscriptionName $subscriptionName
 Set-AzContext $context
+
+# Create Resource Group
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-# Create Storage account
-$srcStorageAccountName = "lwscus$($environment)dfcicdstg1"
-$destStorageAccountName = "lwscus$($environment)dfcicdstg2"
-New-AzStorageAccount -ResourceGroupName $resourceGroupName `
-  -Name $srcStorageAccountName `
-  -Location $location `
-  -SkuName Standard_LRS `
-  -Kind StorageV2 `
-  -AccessTier Hot
-New-AzStorageAccount -ResourceGroupName $resourceGroupName `
-  -Name $destStorageAccountName `
-  -Location $location `
-  -SkuName Standard_LRS `
-  -Kind StorageV2 `
-  -AccessTier Hot
+# Create Src and Dest Storage accounts
+$srcStorageAccountName = "$($orgNameShort)$($locationShort)$($environment)dfcicdstg1"
+$destStorageAccountName = "$($orgNameShort)$($locationShort)$($environment)dfcicdstg2"
+New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $srcStorageAccountName -Location $location -SkuName Standard_LRS -Kind StorageV2 -AccessTier Hot
+New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $destStorageAccountName -Location $location -SkuName Standard_LRS -Kind StorageV2 -AccessTier Hot
 
 #Upload SampleCSVFile1.csv to Source Storage Account
 $srcStorageAccountContext = New-AzStorageContext -StorageAccountName $srcStorageAccountName -StorageAccountKey (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $srcStorageAccountName)[0].Value
@@ -35,7 +25,7 @@ New-AzStorageContainer -Name "data" -Context $srcStorageAccountContext
 Set-AzStorageBlobContent -File "SampleCSVFile1.csv" -Container "data" -Blob "SampleCSVFile1.csv" -Context $srcStorageAccountContext
 
 #Create KeyVault
-$keyVaultName = "lwscus$($environment)dfcicdkeyvault"
+$keyVaultName = "$($orgNameShort)$($locationShort)$($environment)dfcicdkeyvault"
 New-AzKeyVault -ResourceGroupName $resourceGroupName -VaultName $keyVaultName -Location $location -Sku Standard
 
 #Get storage account full connection strings and store them in keyvault
@@ -49,7 +39,7 @@ Set-AzKeyVaultSecret -VaultName $keyVaultName -Name "srcStorageAccountConnection
 Set-AzKeyVaultSecret -VaultName $keyVaultName -Name "destStorageAccountConnectionString" -SecretValue $destStorageAccountConnectionString
 
 # Create Azure Data Factory
-$adfName = "lws-cus-$($environment)-dfcicd-adf"
+$adfName = "$($orgNameShort)-$($locationShort)-$($environment)-dfcicd-adf"
 New-AzDataFactoryV2 -ResourceGroupName $resourceGroupName -Name $adfName -Location $location
 
 # Add Access Policy for Data Factory to access KeyVault
